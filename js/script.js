26951cf426b333910dfe3ecdca0162d9bfdcd1d9
shaheen <shaheenmediusware@gmail.com>
2024-07-11 16:31:17 +0600

    initialize project

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// OTP
+let otp = new VanillaOTP(".otp-input", "#final");
+
+// document.querySelector("#btn-alert").addEventListener("click", function () {
+//   alert(otp.getValue());
+// });
+// document.querySelector("#btn-set").addEventListener("click", function () {
+//   let value;
+//   do {
+//     value = prompt("Please enter an integer value");
+//   } while (value != null && isNaN(value));
+//   otp.setValue(value);
+// });
